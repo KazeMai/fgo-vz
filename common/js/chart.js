feb3a446ad05a988edab40b5bf4e28f2f40f73c4
KazeMai <yichen3354@gmail.com>
2021-08-01 21:25:48 +0800

    新增追加技能及強化素材 、等級上限提高至Lv.120
    mstSvtCoin
    mstSvtAppendPassiveSkill
    mstSvtAppendPassiveSkillUnlock
    mstCombineAppendPassiveSkill

--- common/js/chart.js
+++ common/js/chart.js
@@ -25,7 +25,7 @@
 		}*/
 	var lvMax = 0;
 	if(master.mstSvt[i].type==6) lvMax = master.mstSvtLimit[j].lvMax;
-	else lvMax = /*master.mstSvt[i].rewardLv*/100;
+	else lvMax = /*master.mstSvt[i].rewardLv*/120;
 	for(var c in master.mstSvtExp){
 		if(master.mstSvt[i].expType==master.mstSvtExp[c].type&&master.mstSvtExp[c].lv<=lvMax&&master.mstSvtExp[c].lv>0)
 			data.addRows([[master.mstSvtExp[c].lv,Math.floor(tmpLimit.atkBase+(tmpLimit.atkMax-tmpLimit.atkBase)*master.mstSvtExp[c].curve/1000),Math.floor(tmpLimit.hpBase+(tmpLimit.hpMax-tmpLimit.hpBase)*master.mstSvtExp[c].curve/1000)]]);
